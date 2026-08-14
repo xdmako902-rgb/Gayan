c19a46248126e2a3081c184a7e2d1253bc7134a1
@Mako'Z <xdmako902@gmail.com>
2026-08-15 01:29:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Gayan
+# gayan-MD
---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    PREFIX: "=",
+    OWNER_NUMBER: "94701153310",
+    BOT_NAME: "QUEEN-NESHU-MD",
+    WORK_TYPE: "public",
+
+    AUTO_TYPING: false,
+    AUTO_RECORDING: true,
+    ALWAYS_ONLINE: false,
+    ALWAYS_OFFLINE: true,
+    AUTO_READ: false,
+    READ_CMD_ONLY: true,
+    AUTO_BIO: true,
+
+    AUTO_REACT: false,
+    REACT_EMOJIS: ['❤️','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+
+    AUTO_READ_STATUS: true,
+    AUTO_VIEW_STATUS: true,
+    AUTO_LIKE_STATUS: true,
+    AUTO_LIKE_EMOJI: ['❤️', '🔥', '👍', '😂', '😮'],
+
+    ANTI_BOT: false,
+    ANTI_BAD: false,
+    ANTI_LINK: false,
+    ANTI_DELETE: "true",
+    
+    ANTI_DEL_PATH: "same", 
+    AUTO_RECORDING: "false",
+
+    NEWSLETTER_JID: "7271916507180@lid",
+    GROUP_INVITE_LINK: "https://chat.whatsapp.com/CqDoPKbD49jBxiGb2fLUdt?s=cl&p=a&ilr=0",
+    MAX_RETRIES: 3,
+    RCD_IMAGE_PATH: "https://i.ibb.co/tNZVn1k/IMG-20260710-WA0163.jpg"
+};
